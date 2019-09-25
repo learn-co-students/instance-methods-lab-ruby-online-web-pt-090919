8c3c639f21cd8672201c161cb3b093c40349a206
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-25 21:16:57 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+class Dog #define Dog class
+  def bark #Add instnace method
+    puts "Woof!"
+  end
+  
+  def sit #instance method
+    puts "The Dog is sitting"
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,10 @@
+class Person #define Person class
+  
+  def talk #Add instance method
+    puts "Hello World!"
+  end
+  
+  def walk #Add instnace mehtod
+    puts "The Person is walking"
+  end
+end
